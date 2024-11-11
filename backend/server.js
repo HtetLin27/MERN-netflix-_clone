02071291125adec7089753c199f58023bf5ffa20
htetlynn <htetlynnko27@gmail.com>
2024-11-12 02:58:45 +0700

    setup routes and controller

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,9 @@
 import express from "express";
+import authRoutes from "./routes/auth.route.js";
 
 const app = express();
 
-app.get("/", (req, res) => {
-  res.send("Server is ready 123");
-});
+app.use("/api/v1/auth", authRoutes);
 
 app.listen(5000, () => {
   console.log("Sever started at http://localhost:5000");
