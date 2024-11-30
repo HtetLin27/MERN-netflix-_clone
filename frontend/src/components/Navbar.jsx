01dfe7da30c78c0f21860461880799c3d2d9667f
htetlynn <htetlynnko27@gmail.com>
2024-11-30 23:51:55 +0700

    wip home screen

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -2,6 +2,7 @@ import { LogOut, Menu, Search } from "lucide-react";
 import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import { useAuthStore } from "../store/authUser";
+import { useContentStore } from "../store/content";
 
 const Navbar = () => {
   const [isMobileMenuOpen, setIsMobileMenuOpen] = useState(false);
@@ -10,6 +11,9 @@ const Navbar = () => {
   const toggleMobileMenu = () => {
     setIsMobileMenuOpen(!isMobileMenuOpen);
   };
+
+  const { contentType, setContentType } = useContentStore();
+
   return (
     <header className="max-w-6xl mx-auto flex flex-wrap items-center justify-between p-4 h-20">
       <div className="flex items-center gap-10 z-50">
@@ -22,10 +26,18 @@ const Navbar = () => {
         </Link>
         {/* desktop navbar items */}
         <div className="hidden  sm:flex gap-2 items-center">
-          <Link to={"/"} className="hover:underline">
+          <Link
+            to={"/"}
+            className="hover:underline"
+            onClick={() => setContentType("movie")}
+          >
             Movies
           </Link>
-          <Link to={"/"} className="hover:underline">
+          <Link
+            to={"/"}
+            className="hover:underline"
+            onClick={() => setContentType("tv")}
+          >
             Tv Shows
           </Link>
           <Link to={"/history"} className="hover:underline">
